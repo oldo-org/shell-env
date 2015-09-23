2c9764a0b0dffa1a2d180982cb71fe21da36150e
odoepner <oliver@doepner.net>
2015-09-23 13:19:18 -0300

    fix crlf

--- src/main/scripts/install-toolset-via-ssh.sh
+++ src/main/scripts/install-toolset-via-ssh.sh
@@ -7,6 +7,5 @@ name=shell-env
 ssh $1 -C "svn co https://github.com/guppy4j/${name}/trunk ~/.${name}"
 
 setup_script="~/.${name}/src/main/scripts/setup-symlinks.sh"
-
 ssh $1 -C "chmod u+x ${setup_script}"
 ssh $1 -C "${setup_script}"
