50e7096f53c91ac87d6fe7285242926d768b2f49
odoepner <oliver@doepner.net>
2015-09-23 11:39:59 -0300

    renamed a script

--- src/main/scripts/install-toolset-etc.sh
+++ src/main/scripts/install-toolset-etc.sh
@@ -7,4 +7,3 @@ ssh $1 -C 'ln -s ~/.toolset/src/main/config/etc ~/.etc'
 ssh $1 -C 'ln -s ~/.toolset/src/main/scripts .'
 ssh $1 -C 'chmod ugo+x ~/scripts/*.sh'
 ssh $1 -C '~/scripts/symlink-etc-files.sh'
-
