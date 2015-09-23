5428d1ef5b43220ad06f3f2e530aa42fa9ae9ec5
Oliver Doepner <odoepner@gmail.com>
2015-09-23 13:08:38 -0300

    Update install-toolset-via-ssh.sh

--- src/main/scripts/install-toolset-via-ssh.sh
+++ src/main/scripts/install-toolset-via-ssh.sh
@@ -2,5 +2,5 @@
 
 target=$1
 
-ssh $1 -C 'svn co https://github.com/guppy4j/toolset/trunk ~/.toolset'
-ssh $1 -C '~/.toolset/src/main/scripts/setup-symlinks.sh'
+ssh $1 -C 'svn co https://github.com/guppy4j/shell-env/trunk ~/.shell-env'
+ssh $1 -C '~/.shell-env/src/main/scripts/setup-symlinks.sh'
