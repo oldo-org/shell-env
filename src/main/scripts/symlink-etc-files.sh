33cb74965eb97ffa8ac98fc1a26d1a849541ea28
odoepner <oliver@doepner.net>
2015-09-23 11:25:10 -0300

    take backup copy of original files

--- src/main/scripts/symlink-etc-files.sh
+++ src/main/scripts/symlink-etc-files.sh
@@ -5,6 +5,7 @@ test -d .etc || (echo "~/.etc missing" && exit 1)
 
 symlink() {
   dot_file=".$1"
+  mv --no-clobber "$dot_file" "$dot_file.orig"
   rm -f "$dot_file"
   ln -s ".etc/$1" "$dot_file"
 }
