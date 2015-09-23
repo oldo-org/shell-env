96f86075bc42368f431a809a1f7ed218de5f60aa
odoepner <oliver@doepner.net>
2015-09-23 11:34:31 -0300

    Remote execution of installer steps

--- src/main/scripts/install-toolset-readonly.sh
+++ src/main/scripts/install-toolset-readonly.sh
@@ -1,11 +1,10 @@
 #! /bin/bash -ex
- 
-svn co https://github.com/guppy4j/toolset/trunk ~/.toolset
 
-ln -s ~/.toolset/src/main/config/etc ~/.etc
+target=$1
 
-ln -s ~/.toolset/src/main/scripts .
-chmod ugo+x ~/scripts/*.sh
-
-~/scripts/symlink-etc-files.sh 
+ssh $1 -C 'svn co https://github.com/guppy4j/toolset/trunk ~/.toolset'
+ssh $1 -C 'ln -s ~/.toolset/src/main/config/etc ~/.etc'
+ssh $1 -C 'ln -s ~/.toolset/src/main/scripts .'
+ssh $1 -C 'chmod ugo+x ~/scripts/*.sh'
+ssh $1 -C '~/scripts/symlink-etc-files.sh'
 
