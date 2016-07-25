5a248de06f9ab256bafa3e70abbc1163794f1ac1
odoepner <oliver@doepner.net>
2016-07-25 10:53:27 -0300

    By default use white background

--- src/main/scripts/install-toolset-via-ssh.sh
+++ src/main/scripts/install-toolset-via-ssh.sh
@@ -7,13 +7,14 @@ ssh_run() {
   ssh $target -C "$1"
 }
 
-ssh_run "svn co https://github.com/guppy4j/${name}/trunk ~/.${name}"
+ssh_run "svn co https://github.com/guppy4j/${name}/trunk .${name}"
 
-scripts="~/.${name}/src/main/scripts"
+scripts=".${name}/src/main/scripts"
+etc=".${name}/src/main/config/etc"
 
-ssh_run "chmod u+x ${scripts}/setup-symlinks.sh"
+ssh_run "ln -s --force --backup=simple ${etc} .etc"
+ssh_run "ln -s --force --backup=simple ${scripts} ."
+ssh_run "chmod ugo+x ${scripts}/*.sh"
+
 ssh_run "${scripts}/setup-symlinks.sh"
 
-ssh_run "ln -s --force --backup=simple ~/.{name}/src/main/config/etc .etc"
-ssh_run "ln -s --force --backup=simple ${scripts} ."
-ssh_run "chmod ugo+x ~/scripts/*.sh"
