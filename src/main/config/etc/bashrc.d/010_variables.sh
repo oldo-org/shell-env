91fe6fabe26773495c71dc7f6cae1f7e0b5bebcc
Oliver Doepner <oliver@doepner.net>
2017-01-31 06:43:14 -0400

    Fix a small bug in the shell prompt color codes

--- src/main/config/etc/bashrc.d/010_variables.sh
+++ src/main/config/etc/bashrc.d/010_variables.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-export PS1='\[\e]0;\u@\h \w\a\n\]\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\n\$ '
+export PS1='\[\e]0;\u@\h \w\a\n\]\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\n\$ '
 
 export PATH="$PATH:$HOME/scripts"
