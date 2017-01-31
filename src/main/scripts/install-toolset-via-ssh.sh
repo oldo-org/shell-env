4806237620563f2ba65ebe062b33eb8a6e71c722
Oliver Doepner <oliver@doepner.net>
2017-01-31 06:28:31 -0400

    Make shell scripts executable

--- src/main/scripts/install-toolset-via-ssh.sh
+++ src/main/scripts/install-toolset-via-ssh.sh
@@ -1,13 +1,14 @@
 #! /bin/bash -ex
 
 name=shell-env
+source="https://github.com/guppy4j/shell-env.git"
 target=$1
 
 ssh_run() {
   ssh $target -C "$1"
 }
 
-ssh_run "svn co https://github.com/guppy4j/${name}/trunk .${name}"
+ssh_run "svn co ${source} .${name}" || ssh_run "git clone ${source} .${name}"
 
 scripts=".${name}/src/main/scripts"
 etc=".${name}/src/main/config/etc"
