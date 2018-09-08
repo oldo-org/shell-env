681d6a0c66c07dbaacb82409cce5e2761d68510e
odoepner <oliver@doepner.net>
2018-09-08 14:06:59 -0300

    Use exiftool 'AllDates' shortcut tag for more reliable date extraction (to also support mp4 videos)

--- src/main/scripts/move-cam-files.sh
+++ src/main/scripts/move-cam-files.sh
@@ -3,7 +3,7 @@
 storage=/opt/data/family
  
 move() {
-  exiftool -r -v -ext $2 '-directory<DateTimeOriginal' \
+  exiftool -r -v -ext $2 '-directory<AllDates' \
            -d "${storage}/$3/%Y/%m" "$1"
 }
  
@@ -11,5 +11,6 @@ move() {
 # if unspecified use current directory
 dir=${1:-.}
  
-move $dir AVI videos
-move $dir JPG photos
+move $dir jpg photos
+move $dir avi videos
+move $dir mp4 videos
