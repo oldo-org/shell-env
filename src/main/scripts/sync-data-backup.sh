bb1ae81a57b3872af410a1562f6041debfa42b29
odoepner <oliver@doepner.net>
2018-09-08 11:30:29 -0300

    Make sync script more suitable for execution by cron

--- src/main/scripts/sync-data-backup.sh
+++ src/main/scripts/sync-data-backup.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 bak=/mnt/backup
- 
-sudo umount $bak
-sudo mount $bak
+
+sudo /bin/umount $bak
+sudo /bin/mount $bak
  
 if [ $? -eq 0 ]; then
-  rsync -avvi --progress /opt/data/family/ $bak/family/
-  rsync -avvi --progress $bak/family/ /opt/data/family/
+  rsync -au --progress --size-only /opt/data/family/ $bak/family/
+  rsync -au --progress --size-only --chmod=o-w $bak/family/ /opt/data/family/
 fi
  
-sudo umount $bak
+sudo /bin/umount $bak
