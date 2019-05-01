a843f974b569041f6ab071405887d04c9f3dee71
Oliver Doepner <odoepner@gmail.com>
2019-05-01 20:03:34 -0300

    Create debian.sh

---
+++ src/main/config/etc/bashrc.d/distros/debian.sh
@@ -0,0 +1,15 @@
+alias show='apt-cache show'
+alias search='apt-cache search'
+
+alias files='dpkg -L'
+alias selections='dpkg --get-selections'
+
+alias install='sudo apt-get install'
+alias reinstall='sudo apt-get install --reinstall'
+
+alias update='sudo apt-get update'
+alias upgrade='sudo apt-get upgrade'
+
+alias remove='sudo apt-get remove'
+alias purge='sudo apt-get remove --purge'
+alias autoremove='sudo apt-get autoremove'
