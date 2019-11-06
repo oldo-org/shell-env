b4e0326abe140ec54f2aef5ab7c20be69a68b8b7
odoepner <oliver@doepner.net>
2019-11-06 12:59:20 -0400

    CentOS specific bash setup (wip)

---
+++ src/main/config/etc/bashrc.d/os-specific/centos.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# TODO : Adjust the below to sth that works on vanilla CentOS
+
+#alias show='apt-cache show'
+#alias search='apt-cache search'
+
+#alias files='dpkg -L'
+#alias selections='dpkg --get-selections'
+
+#alias install='sudo apt-get install'
+#alias reinstall='sudo apt-get install --reinstall'
+
+#alias update='sudo apt-get update'
+#alias upgrade='sudo apt-get upgrade'
+
+#alias remove='sudo apt-get remove'
+#alias purge='sudo apt-get remove --purge'
+#alias autoremove='sudo apt-get autoremove'
