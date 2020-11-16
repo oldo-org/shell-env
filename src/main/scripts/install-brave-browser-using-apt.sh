ece7b4a96feae0cd0209c2d24e972210d7f065c7
Oliver Doepner <oliver@thinkpad>
2020-11-16 16:55:56 -0400

    Added shell script marker

--- src/main/scripts/install-brave-browser-using-apt.sh
+++ src/main/scripts/install-brave-browser-using-apt.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 sudo apt install apt-transport-https curl gnupg
 
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
