5ce876e55bad6fae09ec629e018586776403cdc2
odoepner <odoepner@gmail.com>
2020-12-07 23:10:33 -0400

    Let script for MC server.jar installation fail if any step fails

--- src/main/scripts/mc-server-jar-install.sh
+++ src/main/scripts/mc-server-jar-install.sh
@@ -8,14 +8,16 @@ test $# -ne 1 && usage && exit 2
 
 version=$1
 
-cd /opt/minecraft-server/
-
 page="https://mcversions.net/download/$version"
 pattern="https://launcher.mojang.com/v1/objects/.*/server.jar"
 jar="minecraft_server.$version.jar"
 
 server_jar_url="$(wget -O - "$page" | grep -o "$pattern")"
 
+# set shell option for exit on error behavior
+set -e
+
+cd /opt/minecraft-server/
 wget -O "$jar" "$server_jar_url"
 chown minecraft.minecraft "$jar"
 
