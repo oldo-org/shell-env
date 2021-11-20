e602ffcaedf32eab39bb06c5f5c7af06b3f0acb4
odoepner <1536531+odoepner@users.noreply.github.com>
2021-11-19 22:14:52 -0400

    Manage xdg user dirs

--- src/main/scripts/setup-symlinks.sh
+++ src/main/scripts/setup-symlinks.sh
@@ -5,15 +5,20 @@ cd
 test -d .etc || (echo "~/.etc : directory missing" && exit 1)
 
 symlink() {
-  file=".$1"
-  test -e "$file" && mv --no-clobber "$file" "$file.orig"
+  file="$1"
+  test ! -h "$file" -a -f "$file" && mv --no-clobber "$file" "$file.orig"
   rm -f "$file"
-  ln -s ".etc/$1" "$file"
+  ln -s "$2.etc/${file#\.}" "$file"
 }
 
-symlink bashrc
-symlink bash_profile
-symlink minttyrc
-symlink inputrc
-symlink vimrc
+symlink .bashrc
+symlink .bash_profile
+symlink .minttyrc
+symlink .inputrc
+symlink .vimrc
+
+cd .config
+
+symlink user-dirs.dirs "../"
+
 
