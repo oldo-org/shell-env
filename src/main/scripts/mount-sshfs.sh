df4b9139dd97067ef901bd4b8981bdaac174949b
odoepner <1536531+odoepner@users.noreply.github.com>
2021-12-21 07:55:29 -0400

    Fix a typo

--- src/main/scripts/mount-sshfs.sh
+++ src/main/scripts/mount-sshfs.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
  
 for user in $(who -u | cut -d' ' -f1); do
-  home=$(getent passwd ${user} | cut -d':'-f6)
+  home=$(getent passwd ${user} | cut -d':' -f6)
   script="${home}/.sshfs/mount.sh"
   if [ -x "${script}" ]; then
     su ${user} -c ${script}
