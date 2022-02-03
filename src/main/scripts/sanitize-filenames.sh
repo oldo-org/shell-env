f8fc87c4f95732e1680714b867c3251b65d3173d
odoepner <1536531+odoepner@users.noreply.github.com>
2022-02-02 21:50:05 -0400

    Added sanitize-filenames.sh script

---
+++ src/main/scripts/sanitize-filenames.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+ 
+sanitize() {
+  shopt -s extglob;
+ 
+  filename=$(basename "$1" | )
+  directory=$(dirname "$1")
+ 
+  filename_clean="${filename//+([^[:alnum:]_-\.])/_}"
+ 
+  if (test "$filename" != "$filename_clean")
+  then
+    mv -v --backup=numbered "$1" "$directory/$filename_clean"
+  fi
+}
+ 
+export -f sanitize
+find $1 -depth -exec bash -c 'sanitize "$0"' {} \;
