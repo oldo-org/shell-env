ec7d1d9661ec3b050940c798489d1763eb46c0e1
O Doepner <1536531+odoepner@users.noreply.github.com>
2022-10-02 09:34:25 -0300

    Added robot-stopping meta tag

--- src/main/scripts/index-html-img.sh
+++ src/main/scripts/index-html-img.sh
@@ -2,12 +2,13 @@
 echo "<!DOCTYPE html>"
 echo "<html>"
 echo "<head>"
+echo "<meta name="robots" content="noindex,nofollow" />"
 echo "<style>img { float:left;padding:1em;height:15em; }</style>"
 echo "</head>"
 echo "<body>"
 shopt -s nocaseglob nullglob
 for img in *.jpg *.png *.gif; do
-echo "<img src='$img'/>"
+echo "<img src='$img' />"
 done
 echo "</body>"
 echo "</html>"
