7bcb1b6790e0d2ff3a356d72c28988f4a6c2295d
Ol Do <1536531+odoepner@users.noreply.github.com>
2024-02-17 18:44:07 -0400

    Added ~/.local/bin to PATH

--- src/main/config/etc/bashrc.d/010_variables.sh
+++ src/main/config/etc/bashrc.d/010_variables.sh
@@ -2,6 +2,6 @@
 
 export PS1='\[\e]0;\u@\h \w\a\n\]\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\n\$ '
 
-export PATH="$PATH:$HOME/scripts"
+export PATH="$PATH:$HOME/scripts:$HOME/.local/bin"
 
 export EDITOR="vi"
